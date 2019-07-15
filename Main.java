3f93800fee70723ec59c6404969dddba1ab8af62
KDL-K <52216074+KDL-K@users.noreply.github.com>
2019-07-15 12:37:58 +0300

    Main
    Interface access

--- Main.java
+++ Main.java
@@ -5,27 +5,27 @@ public class Main {
     public static void main(String[] args) throws Exception {
 	    ElectricEngine electricEngine = new ElectricEngine();
 	    Body body=new Body("Sedan","silver",4,true);
-	    Vehicle vehicle = new ElectricCar(4,2,5,1500, electricEngine, body);
+	    VehicleControl control = new ElectricCar(4,2,5,1500, electricEngine, body);
 
-	    System.out.println(vehicle.toString());
-	    System.out.println("Is vehicle on = " + vehicle.on());
-        vehicle.turnRight(15);
-        vehicle.turnLeft(0);
-        System.out.println("Is vehicle off = " + vehicle.off());
-        vehicle.openDoor(1);
-        vehicle.closeDoor(1);
+	    System.out.println(control);
+	    System.out.println("Is vehicle on = " + control.on());
+        control.turnRight(15);
+        control.turnLeft(0);
+        System.out.println("Is vehicle off = " + control.off());
+        control.openDoor(1);
+        control.closeDoor(1);
 
         FuelEngine fuelEngine = new FuelEngine(4,1.6f,"petrol",115);
         body=new Body("Coupe","red",3,false);
-        vehicle = new FuelCar(4,2,4,1600, fuelEngine, body);
+        control = new FuelCar(4,2,4,1600, fuelEngine, body);
 
         System.out.println();
-        System.out.println(vehicle.toString());
-        System.out.println("Is vehicle on = " + vehicle.on());
-        vehicle.turnRight(15);
-        vehicle.turnLeft(0);
-        System.out.println("Is vehicle off = " + vehicle.off());
-        vehicle.openDoor(1);
-        vehicle.closeDoor(1);
+        System.out.println(control);
+        System.out.println("Is vehicle on = " + control.on());
+        control.turnRight(15);
+        control.turnLeft(0);
+        System.out.println("Is vehicle off = " + control.off());
+        control.openDoor(1);
+        control.closeDoor(1);
     }
 }
